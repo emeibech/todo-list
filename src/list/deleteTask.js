01ae8f605b2126bd4971b9d6b7baeb3bafab1200
emeibech <emeibech@gmail.com>
2022-11-24 09:29:19 +0800

    Make delete task button functional

---
+++ src/list/deleteTask.js
@@ -0,0 +1,16 @@
+import { returnActiveList } from "../auxiliary/returnActiveList";
+
+//delete task from the dom
+const deleteTaskUI = (e) => {
+    document.querySelector('.task').removeChild(e.target.parentElement.parentElement);
+}
+
+export const deleteTask = (e) => {
+    console.log(returnActiveList().getTasks())
+    if(e.target.localName == 'div') {
+        const task = e.target.parentElement.previousSibling.textContent;
+        returnActiveList().deleteTask(task) //delete task from the object
+        deleteTaskUI(e);
+    }
+    console.log(returnActiveList().getTasks())
+}
--- src/ui/tasks.js
+++ src/ui/tasks.js
@@ -1,5 +1,6 @@
 import '../styles.css'
 import { returnActiveList } from "../auxiliary/returnActiveList";
+import { deleteTask } from "../list/deleteTask";
 
 export const tasks = () => {
     const task = document.createElement('div')
@@ -10,9 +11,14 @@ export const tasks = () => {
         const p = document.createElement(`p`);
         p.textContent = `${returnActiveList().getTasks()[i]}`;
         const button = document.createElement('button')
+        button.classList.add('relative')
         button.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M20 9l-1.995 11.346A2 2 0 0116.035 22h-8.07a2 2 0 01-1.97-1.654L4 9M21 6h-5.625M3 6h5.625m0 0V4a2 2 0 012-2h2.75a2 2 0 012 2v2m-6.75 0h6.75" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>';
+        const overlay = document.createElement('div');
+        overlay.classList.add('absolute', 'w-full', 'h-full')
+        button.appendChild(overlay)
         taskWrapper.append(p, button);
       	task.appendChild(taskWrapper);
     }
     document.querySelector('.wrapper').appendChild(task);
+    document.querySelector('.task').addEventListener('click', deleteTask)
 }
