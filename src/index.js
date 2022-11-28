7da7caf57f7e99b6852c4ccd73178b50ac0c8da3
emeibech <emeibech@gmail.com>
2022-11-28 14:16:10 +0800

    Clean index.js

--- src/index.js
+++ src/index.js
@@ -8,30 +8,3 @@ if (!localStorage.getItem('list')) {
   } else {
     createInstances();
   }
-  
-
-// document.addEventListener('DOMContentLoaded', def);
-
-// if (!localStorage.getItem('list')) {
-//     populateStorage();
-//     def()
-//   } else {
-//     list.push(getList());
-//     def()
-//   }
-
-// const stringedList = []
-// list.forEach(e => {
-//     const obj = {}
-//     obj.name = e.name
-//     obj.tasks = e.getTasks()
-//     obj.crossedOut = e.getCrossedOut()
-//     obj.status = e.getIsActive()
-//     stringedList.push(obj)
-// })
-
-// console.log(stringedList)
-
-// console.log(list)
-// for(let i = 0; i < list.length ; i++) list.pop()
-// console.log(list)
